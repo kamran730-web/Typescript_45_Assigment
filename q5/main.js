95b8d5b38c79962912a489c65653bd98951804c8
Muhammad_Kamran <kk8085528@gmail.com>
2025-10-01 10:17:16 +0500

    q5 commit

---
+++ q5/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = '\n\tBabarAzam\t\n';
+console.log(personName);
+const without_whitespaces = personName.trim();
+console.log(without_whitespaces);
---
+++ q5/main.ts
@@ -0,0 +1,6 @@
+const personName:string = '\n\tBabarAzam\t\n';
+console.log(personName);
+const without_whitespaces:string = personName.trim()
+console.log(without_whitespaces);
+
+
