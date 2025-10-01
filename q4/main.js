61f410b676f0958875da943aaeedb82631fa003d
Muhammad_Kamran <kk8085528@gmail.com>
2025-10-01 10:03:24 +0500

    q4 commit

---
+++ q4/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = "Khabib Nurmagomedov";
+let message = "I never fight for the money. I fight for my legacy, I fight for my people, I fight for my name";
+console.log(`${famous_person} once said ${message}`);
---
+++ q4/main.ts
@@ -0,0 +1,3 @@
+let famous_person:string = "Khabib Nurmagomedov"
+let message:string ="I never fight for the money. I fight for my legacy, I fight for my people, I fight for my name"
+console.log(`${famous_person} once said ${message}`);
