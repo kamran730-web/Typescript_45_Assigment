f501836c1188abffc0ed031f7c0e9e6b2d176e80
Muhammad_Kamran <kk8085528@gmail.com>
2025-10-01 11:14:40 +0500

    q6 commit

---
+++ q6/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// addition
+console.log(6 + 2); //  8 
+// subtraction
+console.log(10 - 2); // 8
+// multiply
+console.log(2 * 4); //  8
+// divide
+console.log(16 / 2); // 8
