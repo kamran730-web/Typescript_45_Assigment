327907f4e4370c424d8e8eaf7c61a73f81104ac2
Muhammad_Kamran <kk8085528@gmail.com>
2025-10-02 09:46:01 +0500

    friend

---
+++ q13/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let guestList = ["Kamran", "Zaryab", "Ahsan"];
+let message = "You are invited to Dinner";
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Mr,${guestList[i]} ${message}`);
+}
---
+++ q13/main.ts
@@ -0,0 +1,6 @@
+let guestList:string[] = ["Kamran","Zaryab","Ahsan"]
+let message:string = "You are invited to Dinner"
+for(let i=0;i<guestList.length;i++){
+    console.log(`Mr,${guestList[i]} ${message}`);
+    
+}
